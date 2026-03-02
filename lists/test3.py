70793d9816645ae0a4dbb04a4fb6bd9e3072b3d0
Bachaspati Nayak <bnayak14900@gmail.com>
2026-03-02 12:19:43 +0530

    started with total=0

--- lists/test3.py
+++ lists/test3.py
@@ -2,15 +2,16 @@
 Given a list of positive integers, return the number of elements that are strictly greater than the average of all previous elements. Skip the first element.
 '''
 def count_greater(list):
-    total=list[0]
+    total=0
     count=0
 
-    for i in range(1,len(list)):
-        avg=total/i
-        if list[i]>avg:
+    for i in range(len(list)):
+        if i!=0:
+          avg=total/i
+          if list[i]>avg:
             print(list[i])
             count+=1
         total+=list[i]
-        return count
+    return count
 
 print(count_greater([11,22,33,44,55,10,4]))
